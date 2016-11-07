dcde38273d0f93ddbbf601b6306403d107e5de81
G Sirlopu <ihaku@the-imac.local>
2016-11-07 15:58:49 -0800

    Initial Commit

---
+++ apiTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apiTester
+//
+//  Created by ihaku on 11/7/16.
+//  Copyright © 2016 sirlopu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
